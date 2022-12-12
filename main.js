61e69d1947f15cd10ea314b5a88c71396e11dbf4
Hannah Papa <hmfloyd@gmail.com>
2022-12-12 09:15:06 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,101 @@
+/*const createChassis = () => {
+    const newchassisObject = {}
+    return newchassisObject
+}
+
+const addWheels = (chassisObject) => {
+    chassisObject.wheels = 4
+    return chassisObject
+}
+
+const addEngine = (chassisObject) => {
+    chassisObject.engine = '4.8L'
+    return chassisObject
+}
+
+const addSteeringWheel = (chassisObject) => {
+    chassisObject.steeringWheel = 'tilting'
+    return chassisObject
+}
+
+const addDriveTrain = (chassisObject) => {
+    chassisObject.driveTrain = 'two wheel drive'
+    return chassisObject
+}
+
+let chassis = createChassis()
+let chassiswithwheels = addWheels(chassis)
+let chassiswithengine = addEngine(chassis)
+let chassiswithsteering = addSteeringWheel(chassis)
+let chassiswithdrivet = addDriveTrain(chassis)
+console.log(chassis)
+*/
+
+/*const buyChocolate = () => {
+    const candyObject = {
+        type: "Millk Chocolate"}
+return candyObject
+}
+
+const addFlavoring = (candyObject) => {
+    candyObject.Flavoring = "Mint"
+    return candyObject
+}
+
+const makeBarkMixture = (candyObject) => {
+    if (candyObject.Flavoring === "Mint"){
+        candyObject.mixed = true
+    }
+    else candyObject.mixed = false 
+    return candyObject
+}
+
+const bakeCandy = (candyObject) => {
+    if (candyObject.mixed === true){
+        candyObject.baked = true
+    }
+    else candyObject.baked = false
+    return candyObject
+}
+
+const breakBark = (candyObject) => {
+    if (candyObject.baked === true){
+        return bark = ["Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece",
+        "Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece", 
+        "Mint Chocolate Bark Piece", "Mint Chocolate Bark Piece"]
+    }
+}
+
+let chocolate = buyChocolate()
+let flavchoc = addFlavoring(chocolate)
+let barkchoc = makeBarkMixture(flavchoc)
+let bakechoc = bakeCandy(barkchoc)
+let breakchoc = breakBark(bakechoc)
+console.log(breakchoc)
+console.log(bark)
+*/
+
+/*const computer = {
+	id: 1,
+	manufacturer: "Apple",
+	make: "Macbook",
+	model: "Pro",
+	specs: {
+		memory: 32,
+		hardDrive: "500 GB",
+		processor: 2.4
+	}
+}
+
+const createHTML = (computer) => {
+    return HTMLstring =`<section id = "computer--${computer.id}">\n
+    \t <h1>${computer.manufacturer} ${computer.make}</h1> \n
+    \t <div>Model: ${computer.model}</div> \n
+    \t <div>Memory: ${computer.specs.memory}GB </div> \n
+    \t <div>Hard drive space: ${computer.specs.hardDrive}</div>\n
+\t <div>Processor speed: ${computer.specs.processor} Ghz</div> \n
+</section>`
+}
+let HTMLresult = createHTML(computer)
+console.log(HTMLresult)*/
+
